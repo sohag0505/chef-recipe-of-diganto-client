d80cd79ff89e9c6392410b1e99d904113f5395c1
sohag838 <95273824+sohag838@users.noreply.github.com>
2023-05-04 16:25:15 +0600

    hide firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,15 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+console.log("environment variable ", import.meta.env.VITE_);
 const firebaseConfig = {
-  apiKey: "AIzaSyBiZVgdIW9lstq80AUKRysZWSRxJcnT6bY",
-  authDomain: "chef-recipe-of-diganto.firebaseapp.com",
-  projectId: "chef-recipe-of-diganto",
-  storageBucket: "chef-recipe-of-diganto.appspot.com",
-  messagingSenderId: "438215105880",
-  appId: "1:438215105880:web:1865ac77615e1654dc192d",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
